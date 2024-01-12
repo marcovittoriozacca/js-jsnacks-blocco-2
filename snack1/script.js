0bb7b7f3acbd72ece8a222965e081585ffba07f4
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-12 13:51:25 +0100

    feat: "while" for the input + push on array + output on console

---
+++ snack1/script.js
@@ -0,0 +1,22 @@
+//array vuoto che conterrà i valori inseriti dall'utente
+const numArray = [];
+
+//variabile che conterrà la somma dei valori inseriti nell'array
+let sumOfNumbers = 0;
+
+//ciclo while - se la somma dei numeri + pari o supererà il valore "50" smetterà di chiedere i valori all'utente
+while(sumOfNumbers < 50){
+
+    //input del valore
+    let userNum = parseInt(prompt("inserisci un numero"));
+
+    //inserimento del valore nell'array
+    numArray.push(userNum);
+
+    //somma numero il a quelli precedentemente inseriti
+    sumOfNumbers += userNum;
+}
+
+//output di tutto l'array e della somma
+console.log(numArray);
+console.log(sumOfNumbers);
